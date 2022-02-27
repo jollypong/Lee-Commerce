00055c6e00cd6a103ec1d32a0b963a1750dbf43c
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-02-27 13:17:38 -0700

    started working on routes

--- models/index.js
+++ models/index.js
@@ -6,23 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  through: '',
-  foreignKey: ''
+  foreignKey: 'category_id'
 });
-// Categories have many Products
-Category.hasMany(Products, {
 
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', 
+  onDelete: 'CASCADE'
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'ProductTag',
-  foreignKey: ''
-})
+  foreignKey: 'product_id'
+});
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
   through: 'ProductTag', 
-  foreignKey: ''
-})
+  foreignKey: 'tag_id'
+});
+
 module.exports = {
   Product,
   Category,
